d843e8b21acf334595a268d0c2b2fe6bc45473d4
RicardoBertolucci <Ric.bertolucci@gmail.com>
2025-05-28 23:33:21 -0300

    creating routes GET and POST

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 import http from "node:http";
-import path from "node:path";
+import { json } from "./src/middleware/json.js";
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
+const task = [];
 
-  res.writeHead(200, { "Content-Type": "application-json" });
-  res.end(
-    JSON.stringify({message: 'Hello World!'})
-  );
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
+
+  const body = await json(req, res);
+
+  if (method == "GET" && url == "/tasks") {
+    res.writeHead(200, { "Content-Type": "application-json" });
+    res.end(JSON.stringify(task));
+    return
+  }
+
+  if (method == "POST" && url == "/tasks") {
+    task.push({
+      title: body.title,
+      description: body.description,
+    });
+
+    res.writeHead(204, { "Content-Type": "application-json" });
+    res.end(JSON.stringify({ message: "Task criada com sucesso" }));
+    return
+  }
+
+  res.writeHead(404, { "Content-Type": "application-json" });
+  res.end(JSON.stringify({ message: "Rota não encontrada!" }));
 });
 
 server.listen(PORT, () => {
