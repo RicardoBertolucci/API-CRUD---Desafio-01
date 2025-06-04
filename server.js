2d4f146cf92fa5b46782177662825b56c2b8634d
RicardoBertolucci <Ric.bertolucci@gmail.com>
2025-06-03 23:30:52 -0300

    update data of POST

--- server.js
+++ server.js
@@ -1,30 +1,42 @@
 import http from "node:http";
 import { json } from "./src/middleware/json.js";
+import { randomUUID } from "node:crypto";
+import { Database } from "./src/models/database.js";
 
 const PORT = 3000;
 
-const task = [];
+const database = new Database();
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
   const body = await json(req, res);
 
-  if (method == "GET" && url == "/tasks") {
-    res.writeHead(200, { "Content-Type": "application-json" });
-    res.end(JSON.stringify(task));
-    return
+  const criaData = (data) => {
+    return data.toLocaleDateString() + " " + data.toLocaleTimeString();
+  };
+
+  if (method == "GET" && url == "/task") {
+    const task = database.select("tasks");
+
+    return res
+      .writeHead(200, { "Content-Type": "application-json" })
+      .end(JSON.stringify(task));
   }
 
   if (method == "POST" && url == "/tasks") {
-    task.push({
+    const task = {
+      id: randomUUID(),
       title: body.title,
       description: body.description,
-    });
+      completed_at: null,
+      created_at: criaData(new Date()),
+      update_at: criaData(new Date()),
+    };
 
-    res.writeHead(204, { "Content-Type": "application-json" });
-    res.end(JSON.stringify({ message: "Task criada com sucesso" }));
-    return
+    database.insert("tasks", task);
+
+    return res.writeHead(204, { "Content-Type": "application-json" }).end();
   }
 
   res.writeHead(404, { "Content-Type": "application-json" });
